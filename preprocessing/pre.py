715c8f2e6268c23f315ac821c954de7d515909d1
Ondřej Ticháček <ondrejtichacek@gmail.com>
2017-06-01 13:49:06 +0200

    New functionality of python preprocessing
    Allow tags such as [Info] [Intro] [Verse] [Bridge] [Chorus]

--- preprocessing/pre.py
+++ preprocessing/pre.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 ''' texify.py converts "chord lines" to injected \chord{} LaTeX tags '''
 
+import os
+import re
 
 def chord_positions(chordline):
     ''' Returns a list of chords and their respective positions in line. '''
@@ -50,5 +52,93 @@ def texify_song(file_location):
             injected_line = inject_line(textlines[i], chords, positions)
             print(injected_line + "\\\\")
 
+def parse_song_info(data):
 
-texify_song('input.txt')
+    song_info = {'Title': [], 'By': [], 'Capo': [], 'Strumming': []}
+
+    for line in data.splitlines():
+        m = re.match(r'Title: (.*)', line)
+        if m:
+            song_info['Title'] = m.group(1)
+
+        m = re.match(r'By: (.*)', line)
+        if m:
+            song_info['By'] = m.group(1)
+
+        m = re.match(r'Capo: (.*)', line)
+        if m:
+            song_info['Capo'] = m.group(1)
+
+        m = re.match(r'Strumming: (.*)', line)
+        if m:
+            song_info['Strumming'] = m.group(1)
+
+    formated = '{{\\Large\\bfseries {Title}}}\\\\\n{{\\large\\bfseries\\itshape {By}}}\\\\\n\\textbf{{Capo}}: {Capo}\\\\\n\\textbf{{Strumming}}: {Strumming}\n'.format(**song_info)
+
+    return formated
+
+def split_song(file_location):
+    ''' Handles anotated txt file. Allowed tags (and file structure):
+        [Info]
+            Title:
+            By:
+            Capo:
+            Strumming:
+
+        [Intro]
+        [Verse]
+        [Chorus]
+        [Bridge]
+    '''
+    with open(file_location, 'r', encoding="utf-8") as f:
+        txt = f.read()
+
+    with open(file_location + '.tex', 'w', encoding="utf-8") as f:
+        song_parts = re.split(r'(\[\w+\])\n', txt)
+
+        if song_parts[0] == '':
+            song_parts.pop(0)
+
+        tags = song_parts[::2]
+        data = song_parts[1::2]
+
+        for j, (tag, dat) in enumerate(zip(tags, data)):
+            if tag.lower() == '[info]':
+                f.write(parse_song_info(dat) + "\n")
+
+            elif tag.lower() == '[intro]':
+                f.write('\\textbf{{Intro}}:\\\\\n' + dat)
+
+            elif tag.lower() == '[bridge]':
+                f.write('\\textbf{{Bridge}}:\\\\\n' + dat)
+
+            elif tag.lower() == '[chorus]' or tag.lower() == '[verse]':
+
+                lines = [s for s in dat.splitlines() if s]
+
+                chordlines = lines[::2]
+                textlines = lines[1::2]
+
+                parsed = ''
+
+                for i in range(len(chordlines)):
+                    chords, positions = chord_positions(chordlines[i])
+                    injected_line = inject_line(textlines[i], chords, positions)
+                    # print(injected_line + "\\\\")
+                    parsed += injected_line + "\\\\\n"
+
+                if tag.lower() == '[chorus]':
+                    f.write('\n\\textbf{{Chorus}}:\\\\\n')
+                elif tag.lower() == '[verse]':
+                    f.write('\n\\textbf{{Verse}}:\\\\\n')
+
+                f.write(parsed)
+
+
+        # with open(file_location + '.tex', 'w', encoding="utf-8") as f:
+        #
+        #     f.write('\n'.join(zip(tags,data)))
+
+# texify_song('input.txt')
+
+split_song('how-to-save-a-life.txt')
